0c4499fab74b8db87568af87f69becc2e0c04a10
ReflectiveObsidian <E0958151@u.nus.edu>
2024-06-23 15:40:02 +0800

    Add call done view

--- view/view.py
+++ view/view.py
@@ -2,33 +2,41 @@ import tkinter as tk
 
 from tkinter import scrolledtext
 
+from view.call_done_view import CallDoneView
+
 class View:
     def __init__(self, master, controller):
         master.geometry("800x800")
         self.controller = controller
+        self.frame = tk.Frame(master)
+        self.call_done_view = CallDoneView(master, back_view=self)
 
-        self.call_logs = scrolledtext.ScrolledText(master, bg='white')
+        self.frame.pack()
+
+        tk.Label(self.frame, text='Call Dashboard', font=("Helvetica", 16)).pack(padx=10, pady=10)
+
+        self.call_logs = scrolledtext.ScrolledText(self.frame, bg='white')
         self.call_logs.pack(padx=10, pady=10)
 
-        self.emotion_label = tk.Label(master, text="Emotion:")
+        self.emotion_label = tk.Label(self.frame, text="Emotion:")
         self.emotion_label.pack(padx=10, pady=10)
 
-        self.emotion = tk.Label(master, text="waiting...", bg='white')
+        self.emotion = tk.Label(self.frame, text="waiting...", bg='white')
         self.emotion.pack()
 
-        self.personalities_label = tk.Label(master, text="Personalities:")
+        self.personalities_label = tk.Label(self.frame, text="Personalities:")
         self.personalities_label.pack(padx=10, pady=10)
 
-        self.personalities = tk.Label(master, bg='white', text="waiting...")
+        self.personalities = tk.Label(self.frame, bg='white', text="waiting...")
         self.personalities.pack(padx=10, pady=10)
 
-        self.warnings_label = tk.Label(master, text="Warnings:")
+        self.warnings_label = tk.Label(self.frame, text="Warnings:")
         self.warnings_label.pack(padx=10, pady=10)
 
-        self.warnings = tk.Label(master, bg='white', text="waiting...")
+        self.warnings = tk.Label(self.frame, bg='white', text="waiting...")
         self.warnings.pack(padx=10, pady=10)
         
-        self.button_frame = tk.Frame(master)
+        self.button_frame = tk.Frame(self.frame)
         self.button_frame.pack(padx=10, pady=10)
 
         self.button = tk.Button(self.button_frame, bg='#99FF99', text="Start Call", command=self.controller.handle_start_call)
@@ -36,6 +44,9 @@ class View:
 
         self.end_button = tk.Button(self.button_frame, bg='#FF9999', text="End Call", command=self.controller.handle_end_call)
         self.end_button.grid(row=0, column=1, padx=10, pady=10)
+
+        tk.Button(self.frame, text='Go to Page 1',
+                  command=self.go_to_call_done_view).pack()
 
 
     def update(self, model):
@@ -46,5 +57,14 @@ class View:
         self.personalities.config(text=model.get_personalities())
         self.warnings.config(text=model.get_warnings())
 
+        self.call_done_view.update(model)
+
     def formatted_call_logs(self, call_logs):
         return "\n\n".join([str(call_log) for call_log in call_logs])
+    
+    def start_page(self):
+        self.frame.pack()
+    
+    def go_to_call_done_view(self):
+        self.frame.pack_forget()
+        self.call_done_view.start_page()
