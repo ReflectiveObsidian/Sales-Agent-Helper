fa9fee8bf160a01da8bfc0d2216de0e45039a33e
ReflectiveObsidian <E0958151@u.nus.edu>
2024-06-27 14:52:27 +0800

    Update llm-chat-processor class and stub implementaion

--- llm_chat_processors/stub/llm_chat_processor_stub.py
+++ llm_chat_processors/stub/llm_chat_processor_stub.py
@@ -1,6 +1,5 @@
 from time import sleep
 from typing import List
-import threading
 
 from llm_chat_processors.llm_chat_processor import LLMChatProcessor
 from llm_chat_processors.prompt_type import PromptType
@@ -22,7 +21,18 @@ class LLMChatProcessorStub(LLMChatProcessor):
         self.chat_log = chatlog # List of ChatLog
         self.enabled = enabled # If enabled, llm will be prompted on chatlog update
 
+        if type == PromptType.WARNINGS:
+            self.min_chat_history = 3
+            self.max_chat_history = 5
+            self.prompt_method = lambda chatlog_string: None
+        elif type == PromptType.TODO:
+            self.min_chat_history = None
+            self.max_chat_history = None
+            self.prompt_method = lambda chatlog_string: None
+
     def chatlog_update_listener(self, chatlog):
+        while self.active:
+            sleep(0.1)
         self.chat_log = chatlog
         if self.enabled:
             self.run()
@@ -34,21 +44,37 @@ class LLMChatProcessorStub(LLMChatProcessor):
         self.enabled = False
 
     def run(self):
-        if not self.active:
-            self.active = True
-            model_callback = self.model_callback
-            type = self.type
-            chat_log = self.chat_log
-            enabled = self.enabled
+        if self.active:
+            print ("LLM is already active")
+        while self.active:
+            sleep(0.1)
+        print ("LLM not active, going ahead")
+        self.active = True
 
-            # PROMPT LLM HERE -----------------------------------
-            # Choose prompt based on self.type
-            sleep(5)
-            if type == PromptType.WARNINGS:
-                model_callback("Hello Warning")
-            elif type == PromptType.TODO:
-                model_callback("Hello Todo")
-            # --------------------------------------------------
+        model_callback = self.model_callback
+        type = self.type
+        chat_log = self.chat_log
+        min_chat_history = self.min_chat_history
+        max_chat_history = self.max_chat_history
 
+        if min_chat_history is not None and len(chat_log) <= min_chat_history:
             self.active = False
+            return
+        if max_chat_history is not None and len(chat_log) > max_chat_history:
+            chat_log = chat_log[-max_chat_history:]
+        chatlog_string = ""
+        for chat in chat_log:
+            chatlog_string += chat.speaker + ": " + chat.content + "\n"
+        chatlog_string = ""
+        # Run prompt_method here -----------------------------------
+        sleep(5)
+        if type == PromptType.WARNINGS:
+            output = self.prompt_method(chatlog_string)["choices"][0]["text"]
+            model_callback(output)
+        elif type == PromptType.TODO:
+            model_callback("Creating to-do list...")
+            output = self.prompt_method(chatlog_string)["choices"][0]["text"]
+            model_callback(output)
+        # --------------------------------------------------
+        self.active = False
 
