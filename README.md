f812ef5752705b9f68d1536af43a80651ceee94c
ReflectiveObsidian <E0958151@u.nus.edu>
2024-06-27 15:51:59 +0800

    Update documentation

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Sales agent helper is a client app for sales agents that helps them maintain eff
 # Requirements
 See requirements.txt for python libraries used.
 Other requirements:
-- cuda toolkit archive 12.1.0
-https://developer.nvidia.com/cuda-12-1-0-download-archive
+- cuda toolkit archive 12.5
+https://developer.nvidia.com/cuda-downloads
 - cuddn for cuda 12.1.0
 https://developer.nvidia.com/downloads/compute/cudnn/secure/8.9.7/local_installers/12.x/cudnn-windows-x86_64-8.9.7.29_cuda12-archive.zip/
 
--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from chat_processors.text2MBTI_chat_processor import Text2MBTIChatProcessor
 
 # LLM Chat Processors
 from llm_chat_processors.stub.llm_chat_processor_stub import LLMChatProcessorStub
+from llm_chat_processors.non_finetuned_llm_chat_processor import NonFinetunedLLMChatProcessor
 
 class Controller:
     def __init__(self):
@@ -33,9 +34,13 @@ class Controller:
         personalities_processor_callback = self.personalities_processor.get_callback()
         self.model.set_call_log_observer(personalities_processor_callback)
 
-        self.llm_chat_processor = LLMChatProcessorStub() # Comment out and change stub to actual
-        llm_chat_processor_callback = lambda calllog: threading.Thread(target=self.llm_chat_processor.chatlog_update_listener(calllog)).start()
+        #self.llm_chat_processor = LLMChatProcessorStub() # Comment out and change stub to actual
+        self.llm_chat_processor = NonFinetunedLLMChatProcessor()
+        llm_chat_processor_callback = lambda calllog: threading.Thread(target=self.llm_chat_processor.chatlog_update_listener, args=[calllog]).start()
         self.model.set_call_log_observer(llm_chat_processor_callback)
+
+        self.call_manager = CallStub(lambda call_log: self.model.add_call_log(call_log)) # To Replace
+        #self.call_manager = WhisperCallManager(lambda call_log: self.model.add_call_log(call_log))
         
         # Configure the grid to expand with the window
         for i in range(5):
@@ -44,13 +49,13 @@ class Controller:
 
     def run(self):
         self.root.title("Sales Helper")
+        self.root.attributes('-topmost', 1)
+        self.root.after_idle(self.root.attributes, '-topmost', 0)
         self.root.mainloop()
 
     def handle_start_call(self):
         self.model.initialise()
 
-        #self.call_manager = CallStub(lambda call_log: self.model.add_call_log(call_log)) # To Replace
-        self.call_manager = WhisperCallManager(lambda call_log: self.model.add_call_log(call_log))
         self.call_manager_thread = threading.Thread(target=self.call_manager.start_call)
         self.call_manager_thread.start()
 
@@ -59,9 +64,6 @@ class Controller:
     def handle_end_call(self):
         if self.call_manager is not None:
             self.call_manager.end_call()
-            self.call_manager_thread.join()
-            self.call_manager = None
-        
         self.llm_chat_processor.set_prompt(PromptType.TODO, self.model.get_call_logs(), lambda todo: self.model.set_todo_list(todo), False)
         self.llm_chat_processor_thread = threading.Thread(target=self.llm_chat_processor.run)
         self.llm_chat_processor_thread.start()
